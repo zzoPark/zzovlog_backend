2dbd1ad0f144ea71b8d810b841f92d54de772f20
zzoPark <rweee408@gmail.com>
2019-05-21 21:31:31 +0900

    added mod_wsgi.server & STATIC_ROOT settings

--- zzovlog/settings.py
+++ zzovlog/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
+    'mod_wsgi.server',
 ]
 
 MIDDLEWARE = [
@@ -131,7 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # rest_framework
 
